9e6a72bb315b7c3e6dd721d9e2bfe5e2867df0c7
Zahoor Butt <zahoor_butt@hotmail.com>
2018-11-09 20:03:04 -0500

    made more changes
    made changes to data access

--- Documents/Visual Studio 2015/Projects/ZB_Profile/ZB_ProfileDataAccess/View_Users.cs
+++ Documents/Visual Studio 2015/Projects/ZB_Profile/ZB_ProfileDataAccess/View_Users.cs
@@ -12,12 +12,11 @@ namespace ZB_ProfileDataAccess
     using System;
     using System.Collections.Generic;
     
-    public partial class sysdiagram
+    public partial class View_Users
     {
-        public string name { get; set; }
-        public int principal_id { get; set; }
-        public int diagram_id { get; set; }
-        public Nullable<int> version { get; set; }
-        public byte[] definition { get; set; }
+        public string Email { get; set; }
+        public string Id { get; set; }
+        public string PhoneNumber { get; set; }
+        public string UserName { get; set; }
     }
 }
